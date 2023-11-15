58c15a1b445487a77cdd7b56739090b7721406c7
thomasmpeters <69325986+thomasmpeters@users.noreply.github.com>
2023-11-14 21:39:51 -0600

    Update RankController.swift

--- Automatic/Automatic/RankController.swift
+++ Automatic/Automatic/RankController.swift
@@ -1,22 +1,57 @@
-//
-//  RankController.swift
-//  Automatic
-//
-//  Created by Nikos Claman on 11/14/23.
-//
-import UIKit
-class RankController: UIViewController {
-<<<<<<< Updated upstream
-    @IBOutlet weak var Ranks: UITabBarItem!
+import SwiftUI
 
-=======
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let testLabel = UILabel()
-        testLabel.text = "Test"
-        testLabel.frame = CGRect(x: 0, y: 100, width: view.frame.width, height: 30)
-        view.addSubview(testLabel)
+struct RankView: View {
+    @State private var selectedSegment = 0 // 0 for hours, 1 for days
+
+    var body: some View {
+        VStack {
+            Picker("Statistics", selection: $selectedSegment) {
+                Text("Hours").tag(0)
+                Text("Days").tag(1)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+
+            Group {
+                Text(rankText(for: .master))
+                Text(rankText(for: .emperor))
+                Text(rankText(for: .silver))
+                Text(rankText(for: .bronze))
+                Text(rankText(for: .legend))
+                Text(rankText(for: .copper))
+                Text(rankText(for: .gold))
+                Text(rankText(for: .beginner))
+            }
+        }
     }
->>>>>>> Stashed changes
+
+    private func rankText(for rank: Rank) -> String {
+        switch rank {
+        case .master:
+            return "Master: \(selectedSegment == 0 ? "1000 Hours" : "1000 Days")"
+        case .emperor:
+            return "Emperor: \(selectedSegment == 0 ? "750 Hours" : "750 Days")"
+        case .silver:
+            return "Silver: \(selectedSegment == 0 ? "200 Hours" : "200 Days")"
+        case .bronze:
+            return "Bronze: \(selectedSegment == 0 ? "100 Hours" : "100 Days")"
+        case .legend:
+            return "Legend: \(selectedSegment == 0 ? "500 Hours" : "500 Days")"
+        case .copper:
+            return "Copper: \(selectedSegment == 0 ? "50 Hours" : "50 Days")"
+        case .gold:
+            return "Gold: \(selectedSegment == 0 ? "300 Hours" : "300 Days")"
+        case .beginner:
+            return "Beginner: \(selectedSegment == 0 ? "5 Hours" : "5 Days")"
+        }
+    }
+
+    enum Rank {
+        case master, emperor, silver, bronze, legend, copper, gold, beginner
+    }
+}
+
+struct RankView_Previews: PreviewProvider {
+    static var previews: some View {
+        RankView()
+    }
 }
