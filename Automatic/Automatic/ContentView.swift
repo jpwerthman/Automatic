2377008d87df605ede6f137bf45eef36a58693b4
jonny werthman <j.p.werthman@wustl.edu>
2023-11-14 21:55:25 -0600

    fixed stuff

--- Automatic/Automatic/ContentView.swift
+++ Automatic/Automatic/ContentView.swift
@@ -1,88 +1,70 @@
-//
-//  ContentView.swift
-//  Automatic
-//
-//  Created by Jonny Werthman on 11/12/23.
-//
-
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+        // Use a TabView to create tabs
+        TabView {
+            // First Tab
+            CalendarViewControllerWrapper()
+                .tabItem {
+                    Image(systemName: "calendar")
+                    Text("Calendar")
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+
+            
+            RankControllerWrapper()
+                .tabItem {
+                    Image(systemName: "rosette")
+                    Text("Ranking")
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+            
+            HabitsViewControllerWrapper()
+                .tabItem {
+                    Image(systemName: "checklist")
+                    Text("Habits")
                 }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+// Use UIViewControllerRepresentable for each view controller
+struct CalendarViewControllerWrapper: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> CalendarViewController {
+        return CalendarViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: CalendarViewController, context: Context) {
+        // Update the view controller if needed
+    }
+}
+
+
+struct RankControllerWrapper: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> RankController {
+        return RankController()
+    }
+
+    func updateUIViewController(_ uiViewController: RankController, context: Context) {
+        // Update the view controller if needed
+    }
+}
+
+
+struct HabitsViewControllerWrapper: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> HabitsViewController {
+        return HabitsViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: HabitsViewController, context: Context) {
+        // Update the view controller if needed
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
+
